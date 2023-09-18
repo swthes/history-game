b32a62354e51fd9df1590019d2f3103bdccd1350
Bruno <brunoradoja2911@gmail.com>
2023-09-19 00:01:39 +0100

    lives added, lives being resetted on reload fixed and quiz reloading on the same page when page refresh

--- client/index.js
+++ client/index.js
@@ -1,9 +1,10 @@
 let quizData = [];
 let currentQuestionIndex = 0;
 let quizTypeSelected = null;
+let lives = localStorage.getItem('lives') ? parseInt(localStorage.getItem('lives'), 10) : 3; // Get lives from localStorage
 
 function startQuiz(quizType) {
-    fetch('http://localhost:3000/home') // Fetching from local server
+    fetch('http://localhost:3000/home')
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok');
@@ -19,15 +20,22 @@ function startQuiz(quizType) {
 
             quizTypeSelected = quizType;
 
-            // Save progress to local storage
+            // Only reset lives if it's a new quiz or they had 0 lives previously
+            if (!localStorage.getItem('lives') || lives <= 0) {
+                lives = 3;
+            }
+
+            updateLivesDisplay();
             localStorage.setItem('currentQuestionIndex', currentQuestionIndex);
             localStorage.setItem('quizType', quizTypeSelected);
 
-            // Hide the initial quiz selection buttons
             document.getElementById("start-buttons").style.display = "none";
 
-            // Show the first question
-            displayQuestion(currentQuestionIndex);
+            if (lives > 0) {
+                displayQuestion(currentQuestionIndex);
+            } else {
+                displayOutOfLivesModal();
+            }
         })
         .catch(error => {
             console.error('There was a problem with the fetch operation:', error.message);
@@ -35,13 +43,17 @@ function startQuiz(quizType) {
 }
 
 function displayQuestion(index) {
+    if (lives <= 0) {
+        return displayOutOfLivesModal();
+    }
+
     let question = quizData[index];
     let optionsDiv = document.getElementById("options");
 
     document.getElementById("question-text").innerText = question.question;
-    optionsDiv.innerHTML = ""; // Clear previous options
+    optionsDiv.innerHTML = "";
 
-    question.options.forEach((option, idx) => {
+    question.options.forEach((option) => {
         let optionBtn = document.createElement("button");
         optionBtn.innerText = option;
         optionBtn.addEventListener("click", () => {
@@ -53,7 +65,13 @@ function displayQuestion(index) {
                     finishQuiz();
                 }
             } else {
-                alert("Wrong answer! Try again.");
+                lives--;
+                updateLivesDisplay();
+                if (lives <= 0) {
+                    displayOutOfLivesModal();
+                } else {
+                    alert("Wrong answer! Try again.");
+                }
             }
         });
         optionsDiv.appendChild(optionBtn);
@@ -62,17 +80,30 @@ function displayQuestion(index) {
     document.getElementById("quiz-section").style.display = "block";
 }
 
+function displayOutOfLivesModal() {
+    document.getElementById("out-of-lives-modal").style.display = "block";
+    document.getElementById("quiz-section").style.display = "none";
+}
+
+function updateLivesDisplay() {
+    document.getElementById("lives").innerText = `Lives: ${lives}`;
+    localStorage.setItem('lives', lives);  // Store lives in localStorage
+}
+
 function finishQuiz() {
     alert("Quiz finished!");
     clearProgress();
     document.getElementById("start-buttons").style.display = "block";
     document.getElementById("quiz-section").style.display = "none";
     currentQuestionIndex = 0;
+    lives = 3;
+    updateLivesDisplay();
 }
 
 function clearProgress() {
     localStorage.removeItem('currentQuestionIndex');
     localStorage.removeItem('quizType');
+    localStorage.removeItem('lives');
 }
 
 document.getElementById('vikings-btn').addEventListener('click', () => startQuiz('vikings'));
@@ -83,6 +114,27 @@ document.getElementById('back-to-home').addEventListener('click', () => {
     document.getElementById("quiz-section").style.display = "none";
     document.getElementById("start-buttons").style.display = "block";
     currentQuestionIndex = 0;
+    lives = 3;
+    updateLivesDisplay();
+});
+
+document.getElementById("retry-quiz").addEventListener("click", () => {
+    document.getElementById("out-of-lives-modal").style.display = "none";
+    clearProgress();
+    startQuiz(quizTypeSelected);
+    currentQuestionIndex = 0;
+    lives = 3;
+    updateLivesDisplay();
+});
+
+document.getElementById("modal-home").addEventListener("click", () => {
+    document.getElementById("out-of-lives-modal").style.display = "none";
+    clearProgress();
+    document.getElementById("quiz-section").style.display = "none";
+    document.getElementById("start-buttons").style.display = "block";
+    currentQuestionIndex = 0;
+    lives = 3;
+    updateLivesDisplay();
 });
 
 window.addEventListener("DOMContentLoaded", () => {
