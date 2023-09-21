a81703180170c6a3966cdd333510c0945ca85a89
Bruno <brunoradoja2911@gmail.com>
2023-09-21 16:59:51 +0100

    added images to the quiz, added an image button with text overlay and added heart image when user clicks wrong answer

--- client/index.js
+++ client/index.js
@@ -76,6 +76,8 @@ class Quiz {
                     }
                 } else {
                     this.lives--;
+                    // disable button after wrong choice
+                    optionBtn.disabled = true;
                     this.updateLivesDisplay();
                     this.displayLifeMessages();
                 
@@ -142,11 +144,35 @@ class Quiz {
             
         }
     }
-    // Function to update the lives display
+
+    // Function to update the lives display with the heart-icon.png image where the image is displayed based on the number of lives and the number of lives as a text under the hearts on the right of the screen
     updateLivesDisplay() {
-        document.getElementById("lives").innerText = `Lives: ${this.lives}`;
+        let livesDiv = document.getElementById("lives");
+        livesDiv.innerHTML = "";
+    
+        let heartDiv = document.createElement("div");
+        heartDiv.classList.add("heart-div");
+    
+        for (let i = 0; i < this.lives; i++) {
+            let heart = document.createElement("img");
+            heart.src = "heart-icon.png";
+            heart.alt = "Heart icon";
+            heart.classList.add("heart-icon");
+            heartDiv.appendChild(heart);
+        }
+    
+        let livesText = document.createElement("p");
+        livesText.innerText = this.lives + " lives remaining";
+        livesText.classList.add("lives-text");
+    
+        livesDiv.appendChild(heartDiv);
+        livesDiv.appendChild(livesText);
         localStorage.setItem('lives', this.lives);
     }
+    
+    
+    
+
     // Function to finish the quiz
     finishQuiz() {
         alert("Quiz finished!");
@@ -338,126 +364,305 @@ class Quiz {
 
  }
     // gets the questions for the character
- getcharacterQuestions(name){
-    const question = this.quizData
-    const newQuestion = [];
+//  getcharacterQuestions(name){
+//     const question = this.quizData
+//     const newQuestion = [];
    
-       console.log(question);
-   switch(name){
-    case 'Napoleon':
-        console.log(name);
-        document.getElementById('character-france').style.display= 'none';
-         question.NapoleonQuestions.forEach((Element)=>{
-         let Q = new Questions(Element.id,Element.category,Element.question,Element.options,Element.answer);
-         newQuestion.push(Q);
-        })
-        this.quizData = newQuestion;
-        this.displayinstructions(name);
+//        console.log(question);
+//    switch(name){
+//     case 'Napoleon':
+//         console.log(name);
+//         document.getElementById('character-france').style.display= 'none';
+//          question.NapoleonQuestions.forEach((Element)=>{
+//          let Q = new Questions(Element.id,Element.category,Element.question,Element.options,Element.answer);
+//          newQuestion.push(Q);
+//         })
+//         this.quizData = newQuestion;
+//         this.displayinstructions(name);
        
-        break;
-    case 'JoanaofArc':
-        console.log(name);
+//         break;
+//     case 'JoanaofArc':
+//         console.log(name);
       
-        question.JoanaofArcQuestions.forEach((Element)=>{
+//         question.JoanaofArcQuestions.forEach((Element)=>{
+//             let Q = new Questions(Element.id,Element.category,Element.question,Element.options,Element.answer);
+//             newQuestion.push(Q);
+//            })
+//            this.quizData = newQuestion;
+//            this.displayinstructions(name);
+//         break;
+//     case 'Halfdan':
+//         console.log(name);
+        
+//         question.HalfdanQuestions.forEach((Element)=>{
+//             let Q = new Questions(Element.id,Element.category,Element.question,Element.options,Element.answer);
+//             newQuestion.push(Q);
+//            })
+//            this.quizData = newQuestion;
+//            this.displayinstructions(name);
+//         break;
+//     case 'LeifErikson':
+//         console.log(name);
+        
+//         question.LeifEriksonQuestions.forEach((Element)=>{
+//             let Q = new Questions(Element.id,Element.category,Element.question,Element.options,Element.answer);
+//             newQuestion.push(Q);
+//            })
+//            this.quizData = newQuestion;
+//            this.displayinstructions(name);
+           
+//         break;
+//     case 'HenryVIII':
+//         console.log(name);
+      
+       
+//        console.log(question.HenryVIIIQuestions.length); 
+//        question.HenryVIIIQuestions.forEach((Element)=>{
+//             let q = new Questions(Element.id,Element.category,Element.question,Element.options,Element.answer);
+//             newQuestion.push(q);
+//           })
+          
+//            this.quizData = newQuestion;
+//            console.log(this.quizData);
+//            this.displayinstructions(name);
+//         break;
+//     case 'Pope':
+//         console.log(name);
+       
+//         question.PopeQuestions.forEach((Element)=>{
+//             let Q = new Questions(Element.id,Element.category,Element.question,Element.options,Element.answer);
+//             newQuestion.push(Q);
+//            })
+//            this.quizData = newQuestion;
+//            this.displayinstructions(name);
+//         break;
+//     case 'JuliusCaeser':
+//         console.log(name);
+        
+//         question.JuliusCaeser.forEach((Element)=>{
+//             let Q = new Questions(Element.id,Element.category,Element.question,Element.options,Element.answer);
+//             newQuestion.push(Q);
+//            })
+//            this.quizData = newQuestion;
+//            this.displayinstructions(name);
+//         break;
+//     case 'AugustusEmperor':
+//         console.log(name);
+        
+//         question.AugustusEmperor.forEach((Element)=>{
+//             let Q = new Questions(Element.id,Element.category,Element.question,Element.options,Element.answer);
+//             newQuestion.push(Q);
+//            })
+//            this.quizData = newQuestion;
+//            this.displayinstructions(name);
+//         break;
+//     case 'Tutankhamun':
+//         console.log(name);
+        
+//         question.Tutankhamun.forEach((Element)=>{
+//             let Q = new Questions(Element.id,Element.category,Element.question,Element.options,Element.answer);
+//             newQuestion.push(Q);
+//            })
+//            this.quizData = newQuestion;
+//            this.displayinstructions(name);
+//         break;
+//     case 'Cleopatra':
+//         console.log(name);
+        
+//         question.Cleopatra.forEach((Element)=>{
+//             let Q = new Questions(Element.id,Element.category,Element.question,Element.options,Element.answer);
+//             newQuestion.push(Q);
+//            })
+//            this.quizData = newQuestion;
+//            this.displayinstructions(name);
+//         break;
+
+//     default:
+//         console.log('no character selected');
+//         break;  
+//    }
+ 
+  
+//  }
+
+getcharacterQuestions(name){
+    const question = this.quizData;
+    const newQuestion = [];
+    const characterImageContainer = document.getElementById('character-image-container');
+
+    // Clear any existing images
+    characterImageContainer.innerHTML = '';
+
+    switch(name) {
+        case 'Napoleon':
+            // Insert the Napoleon image
+            const napoleonImg = document.createElement('img');
+            napoleonImg.src = 'character-images/napolean.jpg';
+            napoleonImg.alt = 'Napolean';
+            characterImageContainer.appendChild(napoleonImg);
+
+            console.log(name);
+            document.getElementById('character-france').style.display= 'none';
+            question.NapoleonQuestions.forEach((Element)=>{
+            let Q = new Questions(Element.id,Element.category,Element.question,Element.options,Element.answer);
+            newQuestion.push(Q);
+            })
+            this.quizData = newQuestion;
+            this.displayinstructions(name);
+            break;
+
+        case 'JoanaofArc':
+            // Insert the Joan of Arc image
+            const joanOfArcImg = document.createElement('img');
+            joanOfArcImg.src = 'character-images/joan-of-arc.jpg';
+            joanOfArcImg.alt = 'Joan of Arc';
+            characterImageContainer.appendChild(joanOfArcImg);
+
+            console.log(name);
+            document.getElementById('character-france').style.display= 'none';
+            question.JoanaofArcQuestions.forEach((Element)=>{
+            let Q = new Questions(Element.id,Element.category,Element.question,Element.options,Element.answer);
+            newQuestion.push(Q);
+            })
+            this.quizData = newQuestion;
+            this.displayinstructions(name);
+            break;
+
+        case 'Halfdan':
+            // Insert the Halfdan image
+            const halfdanImg = document.createElement('img');
+            halfdanImg.src = 'character-images/Halfdan.jpg';
+            halfdanImg.alt = 'Halfdan';
+            characterImageContainer.appendChild(halfdanImg);
+
+            console.log(name);
+        
+            question.HalfdanQuestions.forEach((Element)=>{
             let Q = new Questions(Element.id,Element.category,Element.question,Element.options,Element.answer);
             newQuestion.push(Q);
            })
            this.quizData = newQuestion;
            this.displayinstructions(name);
-        break;
-    case 'Halfdan':
+           break;
+
+       case 'LeifErikson':
+        // Insert the Leif Erikson image
+        const leifEriksonImg = document.createElement('img');
+        leifEriksonImg.src = 'character-images/Leif-Erikson.jjpg.jpeg';
+        leifEriksonImg.alt = 'Leif Erikson';
+        characterImageContainer.appendChild(leifEriksonImg);
+
         console.log(name);
-        
-        question.HalfdanQuestions.forEach((Element)=>{
-            let Q = new Questions(Element.id,Element.category,Element.question,Element.options,Element.answer);
-            newQuestion.push(Q);
-           })
-           this.quizData = newQuestion;
-           this.displayinstructions(name);
-        break;
-    case 'LeifErikson':
-        console.log(name);
-        
+
         question.LeifEriksonQuestions.forEach((Element)=>{
             let Q = new Questions(Element.id,Element.category,Element.question,Element.options,Element.answer);
             newQuestion.push(Q);
            })
-           this.quizData = newQuestion;
-           this.displayinstructions(name);
-           
-        break;
-    case 'HenryVIII':
-        console.log(name);
-      
-       
-       console.log(question.HenryVIIIQuestions.length); 
-       question.HenryVIIIQuestions.forEach((Element)=>{
-            let q = new Questions(Element.id,Element.category,Element.question,Element.options,Element.answer);
-            newQuestion.push(q);
-          })
-          
-           this.quizData = newQuestion;
-           console.log(this.quizData);
-           this.displayinstructions(name);
-        break;
-    case 'Pope':
-        console.log(name);
-       
-        question.PopeQuestions.forEach((Element)=>{
-            let Q = new Questions(Element.id,Element.category,Element.question,Element.options,Element.answer);
-            newQuestion.push(Q);
-           })
-           this.quizData = newQuestion;
-           this.displayinstructions(name);
-        break;
-    case 'JuliusCaeser':
-        console.log(name);
-        
-        question.JuliusCaeser.forEach((Element)=>{
-            let Q = new Questions(Element.id,Element.category,Element.question,Element.options,Element.answer);
-            newQuestion.push(Q);
-           })
-           this.quizData = newQuestion;
-           this.displayinstructions(name);
-        break;
-    case 'AugustusEmperor':
-        console.log(name);
-        
-        question.AugustusEmperor.forEach((Element)=>{
-            let Q = new Questions(Element.id,Element.category,Element.question,Element.options,Element.answer);
-            newQuestion.push(Q);
-           })
-           this.quizData = newQuestion;
-           this.displayinstructions(name);
-        break;
-    case 'Tutankhamun':
-        console.log(name);
-        
-        question.Tutankhamun.forEach((Element)=>{
-            let Q = new Questions(Element.id,Element.category,Element.question,Element.options,Element.answer);
-            newQuestion.push(Q);
-           })
-           this.quizData = newQuestion;
-           this.displayinstructions(name);
-        break;
-    case 'Cleopatra':
-        console.log(name);
-        
-        question.Cleopatra.forEach((Element)=>{
-            let Q = new Questions(Element.id,Element.category,Element.question,Element.options,Element.answer);
-            newQuestion.push(Q);
-           })
-           this.quizData = newQuestion;
-           this.displayinstructions(name);
-        break;
+              this.quizData = newQuestion;
+                this.displayinstructions(name);
+                break;
 
-    default:
-        console.log('no character selected');
-        break;  
-   }
- 
-  
- }
+        case 'HenryVIII':
+            // Insert the Henry VIII image
+            const henryVIIIImg = document.createElement('img');
+            henryVIIIImg.src = 'character-images/henryviii.jpg';
+            henryVIIIImg.alt = 'Henry VIII';
+            characterImageContainer.appendChild(henryVIIIImg);
+
+            console.log(name);
+            console.log(question.HenryVIIIQuestions.length); 
+            question.HenryVIIIQuestions.forEach((Element)=>{
+                let q = new Questions(Element.id,Element.category,Element.question,Element.options,Element.answer);
+                newQuestion.push(q);
+              })
+              
+               this.quizData = newQuestion;
+               console.log(this.quizData);
+               this.displayinstructions(name);
+            break;
+
+        case 'Pope':
+            // Insert the Pope image
+            const popeImg = document.createElement('img');
+            popeImg.src = 'character-images/pope-clementvii.jpeg';
+            popeImg.alt = 'Pope';
+            characterImageContainer.appendChild(popeImg);
+
+            console.log(name);
+            question.PopeQuestions.forEach((Element)=>{
+                let Q = new Questions(Element.id,Element.category,Element.question,Element.options,Element.answer);
+                newQuestion.push(Q);
+               })
+               this.quizData = newQuestion;
+               this.displayinstructions(name);
+            break;
+
+        case 'JuliusCaeser':
+            // Insert the Julius Caesar image
+            const juliusCaeserImg = document.createElement('img');
+            juliusCaeserImg.src = 'character-images/julius-caesar.jpg';
+            juliusCaeserImg.alt = 'Julius Caesar';
+            characterImageContainer.appendChild(juliusCaeserImg);
+
+            console.log(name);
+            question.JuliusCaeser.forEach((Element)=>{
+                let Q = new Questions(Element.id,Element.category,Element.question,Element.options,Element.answer);
+                newQuestion.push(Q);
+               })
+               this.quizData = newQuestion;
+               this.displayinstructions(name);
+            break;
+
+        case 'AugustusEmperor':
+            // Insert the Augustus Emperor image
+            const augustusEmperorImg = document.createElement('img');
+            augustusEmperorImg.src = 'character-images/augustus.jpg';
+            augustusEmperorImg.alt = 'Augustus Emperor';
+            characterImageContainer.appendChild(augustusEmperorImg);
+
+            console.log(name);
+            question.AugustusEmperor.forEach((Element)=>{
+                let Q = new Questions(Element.id,Element.category,Element.question,Element.options,Element.answer);
+                newQuestion.push(Q);
+               })
+               this.quizData = newQuestion;
+               this.displayinstructions(name);
+            break;
+
+        case 'Tutankhamun':
+            // Insert the Tutankhamun image
+            const tutankhamunImg = document.createElement('img');
+            tutankhamunImg.src = 'character-images/tutankamun.jpg';
+            tutankhamunImg.alt = 'Tutankhamun';
+            characterImageContainer.appendChild(tutankhamunImg);
+
+            console.log(name);
+            question.Tutankhamun.forEach((Element)=>{
+                let Q = new Questions(Element.id,Element.category,Element.question,Element.options,Element.answer);
+                newQuestion.push(Q);
+               })
+               this.quizData = newQuestion;
+               this.displayinstructions(name);
+            break;
+
+        case 'Cleopatra':
+            // Insert the Cleopatra image
+            const cleopatraImg = document.createElement('img');
+            cleopatraImg.src = 'character-images/cleopatra.jpg';
+            cleopatraImg.alt = 'Cleopatra';
+            characterImageContainer.appendChild(cleopatraImg);
+
+            console.log(name);
+            question.Cleopatra.forEach((Element)=>{
+                let Q = new Questions(Element.id,Element.category,Element.question,Element.options,Element.answer);
+                newQuestion.push(Q);
+               })
+               this.quizData = newQuestion;
+               this.displayinstructions(name);
+            break;
+
+    }
+}
 
 // character introduction
  characterIntroduction(character) {
